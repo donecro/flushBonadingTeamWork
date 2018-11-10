50fec0349a7ceb9d856d2c9766bfc56e77ffdeb0
咩咩咩咩 <851277543@qq.com>
2018-11-10 11:29:49 +0800

    最终版.

--- app/src/main/java/com/example/aliyunmedia/yes/ok/MainActivity.java
+++ app/src/main/java/com/example/aliyunmedia/yes/ok/MainActivity.java
@@ -24,6 +24,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,14 +33,19 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 	private TextView tv,detail;
 	private Button camerabutton,playbutton,selectvideo;
 	private ProgressBar pb;
 	private String path,objectname;
 	private EditText filename;
 	private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
+	private String typeName;//文件后缀名
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,7 +84,7 @@ public class MainActivity extends ActionBarActivity {
 
     public void beginupload(){
     	//通过填写文件名形成objectname,通过这个名字指定上传和下载的文件
-    	objectname=filename.getText().toString();
+    	objectname=filename.getText().toString()+"."+typeName;
     	if(objectname==null||objectname.equals("")){
     		Toast.makeText(MainActivity.this, "文件名不能为空", 2000).show();
     		return;
@@ -116,6 +122,7 @@ public class MainActivity extends ActionBarActivity {
     					// TODO Auto-generated method stub
 					  tv.setText("UploadSuccess");
 					  pb.setVisibility(ProgressBar.INVISIBLE);
+					initData(objectname);
 					}
     			});
     	    }
@@ -182,6 +189,122 @@ public class MainActivity extends ActionBarActivity {
              }
              path=data;
              String b = path.substring(path.lastIndexOf("/") + 1, path.length());
-             detail.setText(b);
+			 int dot = b.lastIndexOf('.');
+			 if ((dot >-1) && (dot < (b.length() - 1))) {
+				 typeName=b.substring(dot + 1);
+			 }
+
+			 detail.setText(b);
+
          }
+
+	private String logPath;
+	private void initData(String name) {
+		String filePath = "/sdcard/Test/";
+		String fileName = "log.txt";
+		logPath=filePath+fileName;
+		writeTxtToFile(name, filePath, fileName);
+		PutObjectRequest put2 = new PutObjectRequest("miemiemie", fileName, logPath);
+		put2.setProgressCallback(new OSSProgressCallback<PutObjectRequest>() {
+			@Override
+			public void onProgress(PutObjectRequest request, long currentSize, long totalSize) {
+
+			}
+		});
+		//填写OSS外网域名
+		String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+		//填写明文accessKeyId和accessKeySecret
+		OSSCredentialProvider credentialProvider = new OSSPlainTextAKSKCredentialProvider("LTAI4nLjBFf9SsUa", "T3Sr5tbOX6hNKAY532S6AnR9a6lkkP");
+		OSS oss = new OSSClient(getApplicationContext(), endpoint, credentialProvider);
+		OSSAsyncTask task = oss.asyncPutObject(put2, new OSSCompletedCallback<PutObjectRequest, PutObjectResult>() {
+			@Override
+			public void onSuccess(PutObjectRequest request, PutObjectResult result) {
+				Log.d("PutObject", "UploadSuccess");
+				//去UI线程更新UI
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						tv.setText("UploadSuccess");
+						pb.setVisibility(ProgressBar.INVISIBLE);
+					}
+				});
+			}
+			@Override
+			public void onFailure(PutObjectRequest request, ClientException clientExcepion, ServiceException serviceException) {
+				// 请求异常
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						pb.setVisibility(ProgressBar.INVISIBLE);
+						tv.setText("Uploadfile,localerror");
+					}
+				});
+				if (clientExcepion != null) {
+					// 本地异常如网络异常等
+					clientExcepion.printStackTrace();
+				}
+				if (serviceException != null) {
+					// 服务异常
+					tv.setText("Uploadfile,servererror");
+					Log.e("ErrorCode", serviceException.getErrorCode());
+					Log.e("RequestId", serviceException.getRequestId());
+					Log.e("HostId", serviceException.getHostId());
+					Log.e("RawMessage", serviceException.getRawMessage());
+				}
+			}
+		});
+	}
+
+	// 将字符串写入到文本文件中
+	public void writeTxtToFile(String strcontent, String filePath, String fileName) {
+		//生成文件夹之后，再生成文件，不然会出错
+		makeFilePath(filePath, fileName);
+		String strFilePath = filePath+fileName;
+		// 每次写入时，都换行写
+		String strContent = strcontent + "\r\n";
+		try {
+			File file = new File(strFilePath);
+			if (!file.exists()) {
+				Log.d("TestFile", "Create the file:" + strFilePath);
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			}
+			RandomAccessFile raf = new RandomAccessFile(file, "rwd");
+			raf.seek(file.length());
+			raf.write(strContent.getBytes());
+			raf.close();
+		} catch (Exception e) {
+			Log.e("TestFile", "Error on write File:" + e);
+		}
+	}
+
+	// 生成文件
+	public File makeFilePath(String filePath, String fileName) {
+		File file = null;
+		makeRootDirectory(filePath);
+		try {
+			file = new File(filePath + fileName);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+
+	// 生成文件夹
+	public static void makeRootDirectory(String filePath) {
+		File file = null;
+		try {
+			file = new File(filePath);
+			if (!file.exists()) {
+				file.mkdir();
+			}
+		} catch (Exception e) {
+			Log.i("error:", e+"");
+		}
+	}
 }
